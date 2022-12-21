6f50ea4ae18aab1aaf2d8ef064a4408d07789d6e
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2022-12-21 15:27:29 -0500

    workout post route added

--- server/routes/api/workoutRoutes.js
+++ server/routes/api/workoutRoutes.js
@@ -46,3 +46,25 @@ router.get('/:id', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+// CREATE A WORKOUT
+router.post('/', async (req, res) => {
+    try {
+        const workoutData = await Workout.create({
+            user_id: req.session.user_id,
+            workout_name: req.body.workout_name,
+            workout_timer: req.body.workout_timer,
+            rest_timer: req.body.rest_timer,
+            repeat_interval: req.body.repeat_interval,
+            default_workout: req.body.default_workout
+
+        });
+        req.session.save(() => {
+            req.session.logged_in = true;
+            res.status(200).json(workoutData);
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(400).json(err);
+    }
+})
