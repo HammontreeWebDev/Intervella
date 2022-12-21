45a2fb5399353b403d9b6c6f6ebc6b2e3d054492
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2022-12-21 17:40:38 -0500

    user data json added for seeds

---
+++ server/seeds/seeds.js
@@ -0,0 +1,16 @@
+const sequelize = require("../config/connection");
+const { User, Workout } = require("../models");
+const userData = require("./userData.json");
+const workoutData = require("./workoutData.json");
+
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+    await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+    await Workout.bulkCreate(workoutData);
+    process.exit(0);
+};
+
+seedAll();
