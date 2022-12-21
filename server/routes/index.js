fd1aac6b0d48c180a921d553fa2abdc564ef4c8e
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2022-12-21 17:18:38 -0500

    router.use and then send the path for the front end

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,4 +3,9 @@ const router = require('express').Router();
 const api = require('./api');
 router.use('/api', api);
 
+// serve up react front-end in production
+router.use((req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+  });
+
 module.exports = router;
