1ad21773379757a366c1337ce33cb86e52ec98b3
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2022-12-21 12:49:38 -0500

    realized for user auth, we will probably need to go ahead and use the express router. Most navigation will be component based, so we'll authenticate any component 'page' but other than that it will be component based

--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -17,3 +17,22 @@ router.get('/', async (req, res) => {
 });
 
 // CREATE A NEW USER
+router.post('/', async (req, res) => {
+    try {
+        const newUserData = await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        });
+
+        req.session.save(() => {
+            req.session.user_id = newUserData.id;
+            req.session.logged_in = true;
+
+            res.status(200).json(newUserData);
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
