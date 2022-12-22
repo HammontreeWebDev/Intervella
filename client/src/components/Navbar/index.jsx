30360980ce9669aa0396887ef004758b7d7442ca
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2022-12-21 23:50:53 -0500

    cleaned up app.css, added navbar component and rel files

---
+++ client/src/components/Navbar/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './assets/css/navbar.css';
+
+const Navbar = () => {
+
+    return(
+        <header className="custom-header">
+        <nav className="custom-nav">
+            Hello This is the Home page
+        </nav>
+        </header>
+    )
+};
+
+export default Navbar;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
 
-    
+    return(
+        <>
+        <Navbar />
+        </>
+    )
 };
 
 export default Home;
