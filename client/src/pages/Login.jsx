55b8d7a5197565c7889f269742344438ca13463f
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2022-12-23 16:50:45 -0500

    Login page Navbar and footer added

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+import '../assets/css/Login.css';
+
+const Login = () => {
+
+    return(
+        <>
+        <Navbar />
+        <main className="login-main">
+
+        </main>
+        <Footer />
+        </>
+    )
+
+}
+
+export default Login;
