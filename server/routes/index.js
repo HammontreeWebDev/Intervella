258e1ee6e7666e35640290ddce2e7dc759634eda
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2022-12-26 20:41:10 -0500

    going to use express sessions for backend operation authentication and jwt token for front end route authentication

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
 
 const api = require('./api');
-const home = require('./home');
 
-router.use('/', home);
 router.use('/api', api);
 
 module.exports = router;
