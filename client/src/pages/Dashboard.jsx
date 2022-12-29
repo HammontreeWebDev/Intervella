a5e2f895f070510d617a86e73851ab873ab99da6
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2022-12-28 23:30:15 -0500

    dashboard background made

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,19 +4,73 @@ import Footer from "../components/Footer";
 import { Link } from "react-router-dom";
 import Auth from "../utils/auth";
 
+import '../assets/css/Dashboard.css';
+
 // ! any route that has authmiddleware you will need to getToken() to authenticate --> refer to googlebooks HW in api.js --> needs authorization header with bearer token
 
 const Dashboard = () => {
 
-    return(
+    return (
         <>
-        <Navbar
-        home={<Link className='nav-link' to={'/dashboard'}>Home</Link>}
-        logout={<Link className="nav-link" to={'/'} onClick={Auth.logout}>Log Out</Link>}
-        />
-        <main>
-        </main>
-        <Footer />
+            <Navbar
+                home={<Link className='nav-link' to={'/dashboard'}>Home</Link>}
+                logout={<Link className="nav-link" to={'/'} onClick={Auth.logout}>Log Out</Link>}
+            />
+            <main>
+            <div class="background">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            </main>
+            <Footer />
         </>
     )
 }
