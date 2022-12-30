e1007d03cdede24d5296d8ce88060b2809237a88
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2022-12-30 00:28:04 -0500

    form set up, needs styling and functionality for creating a workout

--- client/src/components/CreateWorkout/index.jsx
+++ client/src/components/CreateWorkout/index.jsx
@@ -7,9 +7,34 @@ const CreateWorkout = () => {
 
         <section className="create-section">
             <div className="title-div">
-                <h1 className="workout-title">New Workout</h1>
+                <h2 className="workout-title">New Workout</h2>
                 <button className="edit-button">Edit</button>
             </div>
+            <form className="question-form">
+
+                <div className="grouping">
+                    <label className="question-item">Workout Interval:</label>
+                    <input className="create-input" placeholder="120"/>
+                </div>
+
+                <div className="grouping">
+                    <label className="question-item">Rest Interval:</label>
+                    <input className="create-input" placeholder="30"/>
+                </div>
+
+                <div className="grouping">
+                    <label className="question-item">Repeat (# of times):</label>
+                    <input className="create-input" placeholder="8"/>
+                </div>
+
+                <div className="grouping">
+                    <label className="question-item">Make this my default workout?</label>
+                    <input type="checkbox" />
+                </div>
+
+                <button className="create-button" type="submit">Create Workout</button>
+
+            </form>
         </section>
     )
 }
